4f490a77cefc2c63de2474e71de649658ce1f043
Santiago <satiagovalor90@gmail.com>
2022-12-11 17:09:26 -0300

    variables

--- 01 - Introducción a la Programación/Practica.py
+++ 01 - Introducción a la Programación/Practica.py
@@ -1,1 +1,12 @@
-print('Hola !222')
+
+# Ejercicio 1
+santi=30
+print(f"La edad de santi es {santi}")
+
+# Ejercicio 2
+constante = 8.5
+tipo_const = type(constante)
+
+print(f"El tipo de la constante {constante} es {tipo_const}")
+
+#Ejercicio 3  
